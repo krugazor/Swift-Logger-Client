fde23365ebc086ba557ba2973610dab9d1a7ac78
Mladen_K <kobiljski.mladen@me.com>
2017-11-27 21:22:39 +0100

    v1.0.0
    Initial release

--- README.md
+++ README.md
@@ -1,3 +1,87 @@
-# SwiftLoggerClient
+![](https://img.shields.io/badge/Swift-4.0-orange.svg?style=flat)
+![macOS](https://img.shields.io/badge/os-macOS-green.svg?style=flat)
+![iOS](https://img.shields.io/badge/os-iOS-green.svg?style=flat)
+![Linux](https://img.shields.io/badge/os-linux-green.svg?style=flat)
+![license](https://img.shields.io/github/license/mashape/apistatus.svg?style=flat)
 
-A description of this package.
+
+# Swift Logger Client
+
+Swift Logger Class for Usage With [Swift Logger Server](https://github.com/Mladen-K/Swift-Logger-Server) Application
+
+Simple Logger class used for sending Debug, Info, Warning and Error log messages to the logger API.
+
+
+## Prerequisites
+
+### Swift
+* Open Source Swift 4.0.0 or higher
+
+### macOS
+* macOS Sierra 10.12.6 or higher
+* Xcode Version 9.0 (9A325) or higher
+
+### iOS
+* iOS 10.0 or higher
+
+### Linux
+* Ubuntu 16.04 & 16.10 (only tested on 16.04)
+
+
+## Getting started
+
+```swift
+import SwiftLoggerClient
+```
+
+
+## API
+
+### Public variables & default values
+The following public variables should be set to your desired values.
+```swift
+public let SWIFT_LOGGER_backEndSchema = "http"
+public let SWIFT_LOGGER_backEndPort: Int16 = 80
+```
+
+### Logger class initialization
+```swift
+let log = Logger(applicationName: String, target: Target, onServer: String, state: State)
+```
+
+### Target enum
+```swift
+public enum Target {
+    case file
+    case terminal
+}
+```
+
+### State enum
+```swift
+public enum State {
+    case enabled
+    case disabled
+}
+```
+
+### Exampe usage with viable data:
+```swift
+let log = Logger(applicationName: "MyApplication", target: .file, onServer: "logging.craftwell.io", state: .enabled)
+
+log.debug(message: "This is a debug message.")
+log.info(message: "This is an info message.")
+log.warning(message: "This is a warning message.")
+log.error(message: "An error has occurred.")
+
+// Event can also be logged without passing message parameter:
+log.debug()
+log.info()
+log.warning()
+log.error()
+```
+
+More information about server API configuration is to be found on: [Logging Events Using Swift Cloud Logger](http://craftwell.io/logging-events-using-swift-cloud-logger/)
+
+## License
+[MIT Licence](https://github.com/Mladen-K/Swift-Logger-Client/blob/master/LICENSE)
--- Sources/SwiftLoggerClient/SwiftLoggerClient.swift
+++ Sources/SwiftLoggerClient/SwiftLoggerClient.swift
@@ -18,7 +18,7 @@ import SwiftyJSON
 // http post function that sends the log message to a listening server
 //
 public func sendLog(target: String, appName: String, messageType: String, source: String, line: Int, function: String,  message: String?, server: String) {
-    
+
     let data: [String: Any] = [
         "appName": appName,
         "logType": messageType,
@@ -28,93 +28,106 @@ public func sendLog(target: String, appName: String, messageType: String, source
         "function": function,
         "logText": message ?? ""
     ]
-    
+
     // default backend parameters dependant of Swift Logger Server deployment and configuration
-    let backEndSchema = "http"
+    public let SWIFT_LOGGER_backEndSchema = "http"
+    public let SWIFT_LOGGER_backEndPort: Int16 = 80
     let backEndHost = server
-    let backEndPort: Int16 = 80
     let method = "POST"
     let path = "/logger"
-    
+
     var requestOptions: [ClientRequest.Options] = []
-        requestOptions.append(.schema("\(backEndSchema)://"))
+        requestOptions.append(.schema("\(SWIFT_LOGGER_backEndSchema)://"))
         requestOptions.append(.hostname(backEndHost))
-        requestOptions.append(.port(backEndPort))
+        requestOptions.append(.port(SWIFT_LOGGER_backEndPort))
         requestOptions.append(.method(method))
         requestOptions.append(.path(path))
-    
+
     let headers = ["Content-Type": "application/json"]
     requestOptions.append(.headers(headers))
-    
+
     let req = HTTP.request(requestOptions) { response in
         if let response = response {
             guard response.statusCode == .OK else { return }
             print(response)
         }
     }
-    
+
     let dataAsString = JSON(data).rawString() ?? ""
     req.end(dataAsString)       // req.end initiates request and passes data as body of request
-    
+
 }
 
 //
 // define Logger class
 //
 public class Logger {
-    
+
     // State enum is used to enable/disable logger output
     public enum State {
         case enabled
         case disabled
     }
-    
+
     // Target enum defines weather logger output will be directed to file
     // or terminal only. *in case of file, terminal output won't be disabled
     public enum Target {
         case file
         case terminal
     }
-    
+
     // define instance variables
     private var applicationName: String
     private var state: State
     private var target: String
     private var server: String
-    
-    
+
+
     public init(applicationName: String, target: Target, onServer: String, state: State) {
         self.applicationName = applicationName
         self.state = state
         self.server = onServer
-        
+
         switch target {
         case .file:
             self.target = "file"
         case .terminal:
             self.target = "terminal"
         }
+
+        switch state {
+        case .enabled:
+            self.state = "enabled"
+        case .diabled:
+            self.state = "disabled"
+        }
     }
-    
-    
+
+
     //
     // class methods for different types of log messages
     //
     public func debug(source: String = #file, line: Int = #line, function: String = #function, message: String? = nil) {
-        sendLog(target: target, appName: self.applicationName, messageType: "DEBUG", source: source, line: line, function: function, message: message, server: self.server)
+        if state == "enabled" {
+            sendLog(target: target, appName: self.applicationName, messageType: "DEBUG", source: source, line: line, function: function, message: message, server: self.server)
+        }
     }
-    
+
     public func info(source: String = #file, line: Int = #line, function: String = #function, message: String? = nil) {
-        sendLog(target: target, appName: self.applicationName, messageType: "INFO", source: source, line: line, function: function, message: message, server: self.server)
+        if state == "enabled" {
+            sendLog(target: target, appName: self.applicationName, messageType: "INFO", source: source, line: line, function: function, message: message, server: self.server)
+        }
     }
-    
+
     public func warning(source: String = #file, line: Int = #line, function: String = #function, message: String? = nil) {
-        sendLog(target: target, appName: self.applicationName, messageType: "WARNING", source: source, line: line, function: function, message: message, server: self.server)
+        if state == "enabled" {
+            sendLog(target: target, appName: self.applicationName, messageType: "WARNING", source: source, line: line, function: function, message: message, server: self.server)
+        }
     }
-    
+
     public func error(source: String = #file, line: Int = #line, function: String = #function, message: String? = nil) {
-        sendLog(target: target, appName: self.applicationName, messageType: "ERROR", source: source, line: line, function: function, message: message, server: self.server)
+        if state == "enabled" {
+            sendLog(target: target, appName: self.applicationName, messageType: "ERROR", source: source, line: line, function: function, message: message, server: self.server)
+        }
     }
 }
-
-
